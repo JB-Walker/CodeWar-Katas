987e5d64c9757bb2a85fa2ebfdcf9413b50cb2ba
James Walker <james.walker@contractor.net>
2017-10-28 23:50:24 -0500

    Started the Counting Duplicates Kata. wip

---
+++ Counting_Duplicates.js
@@ -0,0 +1,14 @@
+function duplicateCount (text) {
+    let characters = {};
+    let count = 0;
+    let charMod;
+    for (let char of text) {
+      if (!characters.hasOwnProperty(charMod)) {
+        characters[charMod] = 1;
+      } else if (characters[charMod] === 1) {
+        characters[charMod] = 2;
+        count++;
+      }
+    }
+    return count;
+  }
