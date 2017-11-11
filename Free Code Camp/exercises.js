4d632173f4980d8135b4219417eeb8870381b5f7
James Walker <james.walker@contractor.net>
2017-11-11 16:34:56 -0600

    added exercise 176

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -181,3 +181,15 @@ function testLessThan(val) {
 }
 testLessThan(10);
 
+// 176
+function testLessOrEqual(val) {
+    if (val <= 12) {  // Change this line
+      return "Smaller Than or Equal to 12";
+    }
+    if (val <= 24) {  // Change this line
+      return "Smaller Than or Equal to 24";
+    }
+    return "25 or More";
+}
+testLessOrEqual(10);
+
