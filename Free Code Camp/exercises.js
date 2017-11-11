0e4c39ebee0654345bb83a9b9fb5d2258528e085
James Walker <james.walker@contractor.net>
2017-11-11 16:22:04 -0600

    added exercise 174

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -157,3 +157,15 @@ function testGreaterThan(val) {
 }
 testGreaterThan(101);
 
+// 174
+function testGreaterOrEqual(val) {
+    if (val >= 20) {  // Change this line
+      return "20 or Over";
+    }
+    if (val >= 10) {  // Change this line
+      return "10 or Over";
+    }
+    return "9 or Under";
+}
+testGreaterOrEqual(10);
+
