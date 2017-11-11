acd059768a4d6458edbea4d5865c5e7f6a2d34e9
James Walker <james.walker@contractor.net>
2017-11-10 23:54:24 -0600

    added exercise 149

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -10,3 +10,13 @@ var lastLetterOfLastName = lastName[lastName.length - 1];
 // 148
 var secondToLastLetterOfLastName = lastName[lastName.length - 2];
 
+//149
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = "";
+    // Your code below this line
+    result = "The huge, " + myAdjective + " " + myNoun + " " + myVerb + " around the corner " + myAdverb + "!!";
+  
+    // Your code above this line
+    return result;
+  }
+  
