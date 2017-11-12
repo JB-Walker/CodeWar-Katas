f2ec062038fa244c9687b45669facca65a07be07
James Walker <james.walker@contractor.net>
2017-11-12 01:20:11 -0600

    added Lesson 191

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -386,5 +386,12 @@ var myDog = {
     "friends": ["everything!"]
 };
 
-
+// 191
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };  
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt;
 
