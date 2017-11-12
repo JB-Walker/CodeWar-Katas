aec7c83d2a4d523573b1e17058c825db0e1c94ec
James Walker <james.walker@contractor.net>
2017-11-12 01:08:49 -0600

    added Lesson 189

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -353,4 +353,30 @@ function abTest(a, b) {
   }
   abTest(2,2);
 
-  
+  // 189
+  var count = 0;
+  function cc(card) {
+    switch (card) {
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+        count += 1;
+        break;
+      case 10:
+      case 'J':
+      case 'Q':
+      case 'K':
+      case 'A':
+        count -= 1;
+    }
+    if (count < 1) {
+      return count + " Hold";
+    }
+    return count + " Bet";
+}
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+
