f8d1948ef12aa5469c24d2d16213d15098c996ff
James Walker <james.walker@contractor.net>
2017-11-12 02:55:19 -0600

    lesson 197 in progress

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -441,5 +441,60 @@ var myDog = {
 };
 delete myDog.tails;
 
+// 197
 
+// Setup
+function phoneticLookup(val) {
+    var result = "";
+  
+    // Only change code below this line
+    switch(val) {
+      case "alpha": 
+        result = "Adams";
+        break;
+      case "bravo": 
+        result = "Boston";
+        break;
+      case "charlie": 
+        result = "Chicago";
+        break;
+      case "delta": 
+        result = "Denver";
+        break;
+      case "echo": 
+        result = "Easy";
+        break;
+      case "foxtrot": 
+        result = "Frank";
+    }
+  
+    /*
+    
+  // Setup
+  function phoneticLookup(val) {
+    var result = "";
+    var lookup = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank"
+    };
+    result = lookup.val;
+    return result;
+  }
+  phoneticLookup("charlie");
+    
+    */
+    
+    
+    // Only change code above this line
+    return result;
+  }
+  
+  // Change this value to test
+  phoneticLookup("charlie");
+
+  
   
