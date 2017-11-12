299bd6342c6407226ff86148b4364c8bc3374d14
James Walker <james.walker@contractor.net>
2017-11-12 02:05:59 -0600

    added Lesson 195

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -422,3 +422,12 @@ var myDog = {
 };
 myDog.name = "Happy Coder";
 
+// 195
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Free Code Camp Campers"]
+};
+myDog.bark = "woof";
+
