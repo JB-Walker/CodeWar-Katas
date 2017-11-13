ba992721316c447a197e0cda8fa2cd1a5563f07e
James Walker <james.walker@contractor.net>
2017-11-13 16:47:03 -0600

    Lesson 206

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -557,3 +557,15 @@ for (var i = 0; i < myArr.length; i++) {
   total += myArr[i];
 }
 
+// 206
+function multiplyAll(arr) {
+    var product = 1;
+    for (var i = 0; i < arr.length; i++) {
+      for (var j = 0; j < arr[i].length; j++) {
+        product *= arr[i][j];
+      }
+    }
+    return product;
+}
+multiplyAll([[1,2],[3,4],[5,6,7]]);
+
