e21d8c9cea18a42ab8fc4d858a7b6ebb0a833f05
James Walker <james.walker@contractor.net>
2017-11-14 19:52:04 -0600

    Lesson 218

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -677,3 +677,13 @@ var MotorBike = function() {
     this.engines = 2;
     this.seats = 1;
 };
+
+// 218
+var Car = function() {
+    this.wheels = 4;
+    this.engines = 1;
+    this.seats = 5;
+};  
+var myCar = new Car();
+myCar.nickname = "Zippy";
+
