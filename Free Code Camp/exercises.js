8dc72e74821a4f44030c7f61ea9b5d1b9b0742d6
James Walker <james.walker@contractor.net>
2017-11-16 13:43:50 -0600

    Lesson 235

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -833,3 +833,15 @@ function titleCase (str) {
 }
 titleCase("I'm a little tea pot");
 
+// 235: Return Largest Numbers in Arrays
+function largestOfFour (arr) {
+  let largestArr = arr.map(function(innerArr) {
+    let innerArrSorted = innerArr.sort(function(a, b) {
+      return b - a;
+    });
+    return innerArrSorted[0];
+  });
+  return largestArr;
+}
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
