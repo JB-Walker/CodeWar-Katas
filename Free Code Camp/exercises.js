29a5b5707867eced797ffd914645b6012459a31c
James Walker <james.walker@contractor.net>
2017-11-15 18:34:22 -0600

    Lesson 226

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -753,5 +753,11 @@ var array = [1,2,3,4,5,6,7];
 var newArray = [];
 newArray = array.reverse();
 
+// 226
+var oldArray = [1,2,3];
+var newArray = [];
+var concatMe = [4,5,6];
+newArray = oldArray.concat(concatMe);
+
 
 
