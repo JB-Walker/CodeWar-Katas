15eacc487e57b7e67df46fcac54c5cc3127793a2
James Walker <james.walker@contractor.net>
2017-11-15 18:37:45 -0600

    Lesson 231

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -781,4 +781,16 @@ function reverseString (str) {
 }
 reverseString("hello");
 
+//231: Factorialize a Number
+function factorialize (num) {
+  let factorial = 1;
+  if (num > 1) {
+    for (let i = 2; i <= num; i++) {
+      factorial *= i;
+    }
+  }
+  return factorial;
+}
+factorialize(5);
+
 
