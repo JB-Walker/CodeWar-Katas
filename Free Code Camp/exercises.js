8fa78f740cb091d24046f4492e8f35c8f19de280
James Walker <james.walker@contractor.net>
2017-11-18 13:36:02 -0600

    Lesson 259

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -1310,3 +1310,22 @@ function translatePigLatin(str) {
   }
 }
 translatePigLatin("glove");
+
+// 259: DNA Pairing
+// The DNA strand is missing the pairing element. Take each character, get its pair,
+// and return the results as a 2d array. Base pairs are a pair of AT and CG. Match the
+// missing element to the provided character. Return the provided character as the first
+// element in each array.
+function pairElement(str) {
+  let inputArr = str.split('');
+  let pairs = {'A': 'T',
+               'C': 'G',
+               'G': 'C',
+               'T': 'A'};
+  let outputArr = [];
+  for (let i = 0; i < inputArr.length; i++) {
+    outputArr[i] = [inputArr[i], pairs[inputArr[i]]];
+  }
+  return outputArr;
+}
+pairElement("GCG");
