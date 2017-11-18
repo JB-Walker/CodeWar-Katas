04aefba01a01097d633166880053a1a7f1476f99
James Walker <james.walker@contractor.net>
2017-11-18 13:34:20 -0600

    Lesson 257

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -1279,3 +1279,17 @@ function whatIsInAName(collection, source) {
   return arr;
 }
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+
+// 257: Search and Replace
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+// First argument is the sentence to perform the search and replace on. Second argument is the word that
+// you will be replacing (before). Third argument is what you will be replacing the second argument with (after).
+// NOTE: Preserve the case of the original word when you are replacing it. For example if you mean to replace
+// the word "Book" with the word "dog", it should be replaced as "Dog"
+function myReplace(str, before, after) {
+  if (before[0].match(/[A-Z]/)) {
+    after = after.charAt(0).toUpperCase() + after.substring(1);
+  }
+  return str.replace(before, after);
+}
+myReplace("He is Sleeping on the couch", "Sleeping", "sitting");
