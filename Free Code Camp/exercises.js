8196e30ed82339e4d61e8f5565922ebd5ec93f19
James Walker <james.walker@contractor.net>
2017-11-18 13:36:59 -0600

    Lesson 260

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -1329,3 +1329,16 @@ function pairElement(str) {
   return outputArr;
 }
 pairElement("GCG");
+
+// 260: Missing letters
+// Find the missing letter in the passed letter range and return it.
+// If all letters are present in the range, return undefined.
+function fearNotLetter(str) {
+  for (let i = 1; i < str.length; i++) {
+    if (str.charCodeAt(i - 1) + 1 !== str.charCodeAt(i)) {
+      return String.fromCharCode(str.charCodeAt(i - 1) + 1);
+    }
+  }
+  return undefined;
+}
+fearNotLetter("abce");
