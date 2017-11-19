d1f03115273068f7082139dc7dd891491853fa6e
James Walker <james.walker@contractor.net>
2017-11-19 02:27:06 -0600

    Lesson 263

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -1376,3 +1376,31 @@ function uniteUnique(arr) {
   return concatedArrays;
 }
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+// 263: Convert HTML Entities
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+function convertHTML(str) {
+  let arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '&':
+        arr[i] = '&amp;';
+        break;
+      case '<':
+        arr[i] = '&lt;';
+        break;
+      case '>':
+        arr[i] = '&gt;';
+        break;
+      case '"':
+        arr[i] = '&quot;';
+        break;
+      case "'":
+        arr[i] = '&apos;';
+        break;
+    }
+  }
+  str = arr.join('');
+  return str;
+}
+convertHTML("Dolce & Gabbana");
