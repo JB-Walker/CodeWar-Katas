fc510188869119679212406e6a2c9b515323e412
James Walker <james.walker@contractor.net>
2017-11-20 13:14:28 -0600

    Tribute: Fixed syntax, added comments. Initial images displayed, but not changing

--- Free Code Camp/Tribute/js/script.js
+++ Free Code Camp/Tribute/js/script.js
@@ -3,17 +3,18 @@ window.onload = function start() {
     const  containerCount = 14, imgsPerContainer = 7;
     slideShow();
   }
-  
-// Infinite Loops to continuously change pictures
-function slideShow () {
-  const id, imgNum, imgNumPath;
-  for (let container = 0; container < containerCount; container++) {
-    window.setInterval(function () {
-    id = 'container-' + container;
-    imgNum = Math.floor(Math.random() * imgsPerContainer) + 
-    imgNum = currentCycle * containerCount + container;
-    imgNumPath = 'img/' + imgNum + '.jpg';
-    document.getElementById(id).setAttribute('src', imgNumPath);
-    }, 3000); // repeat forever, changing image name every 3 seconds
+  // There are 14 pic containers, which continuously & ramdonly select
+  // one of seven pictures assigned to that container. After being displayed
+  // for a few seconds the pic changes. The changes not not all happen at once.
+  function slideShow () {
+    const id, imgNum, imgNumPath;
+    for (let container = 0; container < containerCount; container++) {
+      window.setInterval(function () {
+        id = `container-${container}`;
+        imgNum = Math.floor(Math.random() * imgsPerContainer) + container * imgsPerContainer;
+        imgNumPath = `img/${imgNum}.jpg`;
+        document.getElementById(id).setAttribute('src', imgNumPath);
+      }, 3000); // repeat forever, changing image name every 3 seconds
+    }
   }
-}
+  
