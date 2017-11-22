a534c432fc4f5628c7ab035418207444cc2c5122
James Walker <james.walker@contractor.net>
2017-11-22 12:55:40 -0600

    Tribute - script - changed the title to transistion to text after delay using timer

--- Free Code Camp/Projects/Tribute/js/script.js
+++ Free Code Camp/Projects/Tribute/js/script.js
@@ -1,21 +1,30 @@
-// Tribute
 window.onload = function start() {
-    const containerCount = 14; // Number of child pics displayed simultaneously
-    const imgsPerContainer = 7; // Number of unique images available to each container
-    slideShow(containerCount, imgsPerContainer);
+  const containerCount = 14; // Number of child pics displayed simultaneously
+  const imgsPerContainer = 7; // Number of unique images available to each container
+  slideShow(containerCount, imgsPerContainer);
+  showText();
+}
+
+// Each pic container continuously & ramdonly selects a new pic to be displayed from a
+// set of assigned pictures. The pic changes should not not all happen at once.
+function slideShow (containerCount, imgsPerContainer) {
+  for (let container = 0; container < containerCount; container++) {
+    window.setInterval(function () {
+      const id = `container-${container}`;
+      const imgNum = Math.floor(Math.random() * imgsPerContainer) + container * imgsPerContainer;
+      const imgNumPath = `img/${imgNum}.jpg`;
+      document.getElementById(id).setAttribute('src', imgNumPath);
+    }, 1000 + container * 333); // repeat forever, changing image name every 2-4 seconds
   }
-  
-  // Each pic container continuously & ramdonly selects a new pic to be displayed from a
-  // set of assigned pictures. The pic changes should not not all happen at once.
-  function slideShow (containerCount, imgsPerContainer) {
-        for (let container = 0; container < containerCount; container++) {
-          window.setInterval(function () {
-            const id = `container-${container}`;
-            const imgNum = Math.floor(Math.random() * imgsPerContainer) + container * imgsPerContainer;
-            const imgNumPath = `img/${imgNum}.jpg`;
-            document.getElementById(id).setAttribute('src', imgNumPath);
-          }, 1000 + container * 333); // repeat forever, changing image name every 2-4 seconds
-        }
-  }
-  
-  
+}
+
+// Replace the childhood title pics with the promotion pics
+function showText () {
+  window.setTimeout(function () {
+    document.getElementById('titleRight').style.visibility = 'hidden';
+    document.getElementById('textRight').style.display = 'block';
+    document.getElementById('titleLeft').style.visibility = 'hidden';        
+    document.getElementById('textLeft').style.display = 'block';  
+  }, 1000);
+}
+
