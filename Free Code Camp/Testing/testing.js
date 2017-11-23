ed218e5ed7f0e8560b34bac1d43487bd23c26a9f
James Walker <james.walker@contractor.net>
2017-11-22 21:32:17 -0600

    Testing alternative algoritm in sandbox

--- Free Code Camp/Testing/testing.js
+++ Free Code Camp/Testing/testing.js
@@ -1,19 +1,12 @@
-function truthCheck1(collection, pre) {
-  return collection.every(function(element) {
-    return element[pre] === true || element[pre] == true;
+function whatIsInAName(collection, source) {
+  var matchedObjects = [];
+  collection.forEach(function(objectArgument) {
+    var objectProps = Object.keys(objectArgument);
+    for (var key in source) {
+      if (objectProps.includes(key) && objectArgument[key] === source[key]) {
+      }
+    }
   });
+  return matchedObjects;
 }
-console.log("(element[pre] === true || element[pre] == true)");
-console.log("True: "+ truthCheck1([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
-console.log("True: "+ truthCheck1([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat"));
-console.log("True: "+ truthCheck1([{"single": "yes"}], "single"));
-
-function truthCheck2(collection, pre) {
-  return collection.every(function(element) {
-    return element[pre];
-  });
-}
-console.log("(element[pre])");
-console.log("True: "+ truthCheck2([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
-console.log("True: "+ truthCheck2([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat"));
-console.log("True: "+ truthCheck2([{"single": "yes"}], "single"));
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
