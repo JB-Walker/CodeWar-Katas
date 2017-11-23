25216cf3b1b3ded636dabf6e7a10b7fa977d423d
James Walker <james.walker@contractor.net>
2017-11-22 23:04:17 -0600

    Tribute for codePen js and css (no img)

---
+++ Free Code Camp/Projects/Tribute/codePen/js/script.js
@@ -0,0 +1,31 @@
+window.onload = function start() {
+    const containerCount = 14; // Number of child pics displayed simultaneously
+    const imgsPerContainer = 7; // Number of unique images available to each container
+    slideShow(containerCount, imgsPerContainer);
+    showText();
+  }
+  
+  // Each pic container continuously & ramdonly selects a new pic to be displayed from a
+  // set of assigned pictures. The pic changes should not not all happen at once.
+  function slideShow (containerCount, imgsPerContainer) {
+    for (let container = 0; container < containerCount; container++) {
+      window.setInterval(function () {
+        const id = `container-${container}`;
+        const imgNum = Math.floor(Math.random() * imgsPerContainer) + container * imgsPerContainer;
+        const imgNumPath = `http://letsmaketheworldabetterplace.org/img/${imgNum}.jpg`;
+        document.getElementById(id).setAttribute('src', imgNumPath);
+      }, 1000 + container * 333); // repeat forever, changing image name every 2-4 seconds
+    }
+  }
+  
+  // Replace the childhood title pics with the promotion pics
+  function showText () {
+    window.setTimeout(function () {
+      document.getElementById('titleRightPic').style.visibility = 'hidden';
+      document.getElementById('titleRightText').style.display = 'block';
+      document.getElementById('titleLeftPic').style.visibility = 'hidden';        
+      document.getElementById('titleLeftText').style.display = 'block';  
+    }, 3500);
+  }
+  
+  
