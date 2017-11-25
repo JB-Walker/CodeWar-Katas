77d203ea5c9846d617e188d1957506628939f932
James Walker <james.walker@contractor.net>
2017-11-24 23:13:33 -0600

    Lesson 290

--- Free Code Camp/exercises.js
+++ Free Code Camp/exercises.js
@@ -1696,3 +1696,7 @@ truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "ma
 // Q1: Digital files can be made from analog sounds and analog sounds can be made from digital files. True.
 // Q2: Compression is not worth it, because it lessens quality. False.
 
+// 290: Video Challenge - Routers and Packets
+// Q1: Routers have to know the whole internet. False.
+// Q2: Packets are little pieces of binary code. True.
+// Q3: Packets have to be sent in one piece. False.
