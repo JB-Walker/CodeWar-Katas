04631f865441f36602e3c0756910be4c8e8de844
James Walker <james.walker@contractor.net>
2017-11-25 21:17:30 -0600

    added quote group 1

---
+++ Free Code Camp/Projects/Quote_Machine/js/script.js
@@ -0,0 +1,25 @@
+A big fortune will descend upon you this year.
+A chance meeting opens new doors to success and friendship.
+A dream you have will come true.
+A friend asks only for your time not your money.
+A good way to keep healthy is to eat more Chinese food.
+A journey of a thousand miles begins with a single step.
+A letter of great importance may reach you any day now.
+A lifetime of happiness is in store for you.
+A man is born to live and not prepare to live.
+A merry heart does good like a medicine.
+A new business venture is on the horizon.
+A new voyage will fill your life with untold memories.
+A short stranger will soon enter your life with blessings to share.
+A single conversation with a wise man is better than ten years of study.
+A smile is your passport into the hearts of others.
+A stranger, is a friend you have not spoken to yet.
+A very attractive person has a message for you.
+Accept your past without regrets. Handle your present with confidence. Face your future without fear.
+Adversity is the parent of virtue.
+All progress occurs because people dare to be different.
+All your fingers can't be of the same length.
+An upward movement initiated in time can counteract fate.
+Ask yourself if what you are doing today is getting you closer to where you want to be tomorrow.
+Back away from individuals who are impulsive.
+Be on the lookout for coming events; They cast their shadows beforehand.
