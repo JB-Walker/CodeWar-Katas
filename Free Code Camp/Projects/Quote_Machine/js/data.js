883e851d037ae39282eb16a443951d2046db8560
James Walker <james.walker@contractor.net>
2017-11-30 23:52:58 -0600

    data added for table

---
+++ Free Code Camp/Projects/Quote_Machine/js/data.js
@@ -0,0 +1,6 @@
+// Data to be added to table
+Containing 70 percent or more by weight of silk or silk waste (735)
+Containing 70 percent or more by weight of silk or silk waste (736)
+Of fabrics formed on a base of paper or covered or lined with paper
+Containing 70 percent or more by weight of silk or silk waste
+
