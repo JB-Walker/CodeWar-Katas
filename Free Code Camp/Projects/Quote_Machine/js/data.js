d04ebebbdd7ae72f7948a100b2b094dc091f92b0
James Walker <james.walker@contractor.net>
2017-11-30 23:53:38 -0600

    data added for table

--- Free Code Camp/Projects/Quote_Machine/js/data.js
+++ Free Code Camp/Projects/Quote_Machine/js/data.js
@@ -3,4 +3,8 @@ Containing 70 percent or more by weight of silk or silk waste (735)
 Containing 70 percent or more by weight of silk or silk waste (736)
 Of fabrics formed on a base of paper or covered or lined with paper
 Containing 70 percent or more by weight of silk or silk waste
+Containing 70 percent or more by weight of silk or silk waste
+Other gloves, mittens and mitts, all the foregoing specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts
+Other gloves, mittens and mitts, all the fore- going specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts
+Other gloves, mittens and mitts, all the fore- going specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts
 
