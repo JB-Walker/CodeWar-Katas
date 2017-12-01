334242c93582c24f31e417d9b559720d71445e57
James Walker <james.walker@contractor.net>
2017-11-30 23:55:35 -0600

    data added for table

--- Free Code Camp/Projects/Quote_Machine/js/data.js
+++ Free Code Camp/Projects/Quote_Machine/js/data.js
@@ -7,4 +7,13 @@ Containing 70 percent or more by weight of silk or silk waste
 Other gloves, mittens and mitts, all the foregoing specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts
 Other gloves, mittens and mitts, all the fore- going specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts
 Other gloves, mittens and mitts, all the fore- going specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts
+Headbands, ponytail holders and similar articles
+Containing 85 percent or more by weight of silk or silk waste
+Certified hand-loomed and folklore products
+Wall hangings of jute
+Certified hand-loomed and folklore pillow covers of wool or fine animal hair
+Of other textile materials
+Wall banners, of man-made fibers
+Other
+Men's or boys' overcoats, carcoats, capes, cloaks, anoraks (including ski-jackets), windbreakers and   similar articles (including padded, sleeveless jackets), other than those of heading 6203:
 
