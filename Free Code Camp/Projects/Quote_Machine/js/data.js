b4b15c45b0b62eac6584a5cdad16cc63119a7564
James Walker <james.walker@contractor.net>
2017-12-01 23:29:21 -0600

    Edited Tariff Text

--- Free Code Camp/Projects/Quote_Machine/js/data.js
+++ Free Code Camp/Projects/Quote_Machine/js/data.js
@@ -3,12 +3,9 @@ const tariffText = [
 "Of fabrics formed on a base of paper or covered or lined with paper".
 "Containing 70 percent or more by weight of silk or silk waste",
 "Containing 70 percent or more by weight of silk or silk waste",
-Other gloves, mittens and mitts, all the foregoing specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts
-Other gloves, mittens and mitts, all the fore- going specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts
-Other gloves, mittens and mitts, all the fore- going specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts
-Headbands, ponytail holders and similar articles
-Containing 85 percent or more by weight of silk or silk waste
-Certified hand-loomed and folklore products
+"Other gloves, mittens and mitts, all the foregoing specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts".
+"Other gloves, mittens and mitts, all the fore- going specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts".
+"Other gloves, mittens and mitts, all the fore- going specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts".
 Wall hangings of jute
 Certified hand-loomed and folklore pillow covers of wool or fine animal hair
 Of other textile materials
