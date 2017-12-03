7847c0eafb1af864daefee113f568093032c121b
James Walker <james.walker@contractor.net>
2017-12-02 22:58:46 -0600

    editing Text

--- Free Code Camp/Projects/Quote_Machine/js/data.js
+++ Free Code Camp/Projects/Quote_Machine/js/data.js
@@ -1,8 +1,6 @@
 // Data to be added to table
 const tariffText = [
-"Of fabrics formed on a base of paper or covered or lined with paper".
 "Containing 70 percent or more by weight of silk or silk waste",
-"Other gloves, mittens and mitts, all the fore- going specially designed for use in sports, including   ski and snowmobile gloves, mittens and mitts".
 "Wall hangings of jute",
 "Of other textile materials",
 "Wall banners, of man-made fibers",
