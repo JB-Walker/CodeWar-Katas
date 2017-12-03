a1809b003dc6b876eb3c8a1ae62f8f25085a0375
James Walker <james.walker@contractor.net>
2017-12-02 23:00:22 -0600

    editing Text

--- Free Code Camp/Projects/Quote_Machine/js/data.js
+++ Free Code Camp/Projects/Quote_Machine/js/data.js
@@ -1,17 +1,9 @@
 // Data to be added to table
 const tariffText = [
-"Containing 70 percent or more by weight of silk or silk waste",
-"Wall hangings of jute",
-"Of other textile materials",
-"Wall banners, of man-made fibers",
 "Men's (434)",
 "Boys' (434)",
-"Other",
 "Men's (334)",
-"Other:",
 "Men's (334)",
 "Boys' (334)",
-"Other:",
 "Men's (334)",
-"Containing 15 percent or more by weight of down and waterfowl plumage",
 ]
