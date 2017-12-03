4d7b13c137c4a70e6efcae694635102bd25cc7ef
James Walker <james.walker@contractor.net>
2017-12-02 22:55:35 -0600

    editing Text

--- Free Code Camp/Projects/Quote_Machine/js/data.js
+++ Free Code Camp/Projects/Quote_Machine/js/data.js
@@ -17,11 +17,10 @@ const tariffText = [
 "Men's (334)",
 "Other:",
 "Men's (334)",
-Boys' (334)
-Other:
+"Boys' (334)",
+"Other:",
 Men's (334)
 Of man-made fibers:
 Containing 15 percent or more by weight of down and waterfowl plumage and of which down comprises 35   percent or more by weight; containing 10 percent or more by weight of down (653)
-Other:
 Containing 36 percent or more by weight of wool or fine animal hair
 ]
