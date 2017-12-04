8ef2eb68c71ed2cee04eb27eb96b07c082e279c7
James Walker <james.walker@contractor.net>
2017-12-03 23:27:47 -0600

    Editing array TariffText array info

--- Free Code Camp/Projects/Quote_Machine/js/data.js
+++ Free Code Camp/Projects/Quote_Machine/js/data.js
@@ -2,14 +2,9 @@
 const tariffText = [
     ['U.S.', 'exports', 'of', 'domestic', 'exports', 'merchandise'],
     ['China', '1,083', '1,242',	'1,243', '1,350', '1,231', '-119', '-8.8'],
-    India	141	162	167	165	171	7	4
-    Canada	3,430	3,710	3,917	3,905	3,906	1	(a)
-    Indonesia	113	132	142	178	167	-10	-5.8
-    Bangladesh	8	20	28	22	21	-2	-8.3
-    Honduras	1,468	1,850	1,465	1,408	1,541	133	9.4
-    Pakistan	55	40	32	40	34	-6	-16
-    Italy	152	171	151	143	149	6	4.1
-    All other	7,196	8,041	7,876	8,009	8,047	38	0.5
+    India	141	162	167	
+    Canada	3,430	3,710	3,917	3,905	3,906
+ 
     Total domestic exports	17,366	19,475	19,251	19,705	19,992	287	1.5
     Re-exports	2,350	2,897	3,221	3,619	3,924	305	8.4
     Total U.S. exports (domestic exports and re-exports)	19,716	22,372	22,472	23,324	23,916	592	2.5
@@ -17,5 +12,12 @@ const tariffText = [
       <title>Quote Machine of Sorts</title>
   <link rel="stylesheet" type="text/css" href="css/style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <img src="img/cookie.jpg" alt="Fortune cookie fortune display" usemap="#fortune"/>
+  <map name="fortune">
+    <area shape="rect" coords="0,0,1500,999" id="clicker">
+  </map>
+  <div id="fortuneBox">
+    A Smile is Your Passport into the hearts of others
+  </div>
  
 ]
