d93b7ff588bf08346746f7fda7e0838b67a267dd
James Walker <james.walker@contractor.net>
2017-12-04 23:35:39 -0600

    Editing array TariffText array info

--- Free Code Camp/Projects/Quote_Machine/js/data.js
+++ Free Code Camp/Projects/Quote_Machine/js/data.js
@@ -2,12 +2,12 @@
 const tariffText = [
     [1, 'U.S.', 'exports', 'of', 'domestic', 'exports', 'merchandise'],
     [2, 'China', '1,083', '1,242',	'1,243', '1,350', '1,231', '-119', '-8.8'],
-    [3, 'India', 141, 162, 167],
-    [4, 'Canada', 3430, 3710, 3917, 3905, 3906],
+    [3, 'India 141, 162, 167'],
+    [4, 'Canada 3430, 3710, 3917, 3905, 3906'],
     [5, 'Total domestic exports	17366	19475	19251	19705	19992	287	1.5'],
     [6, 'Re-exports	2,350	2,897	3,221	3,619	3,924	305	8.4'],
     [7, 'Total U.S. exports (domestic exports and re-exports)	19,716	22,372	22,472	23,324	23,916	592	2.5'],
     [8, 'China	42,195	44,884	45,066	46,469	47,204	735	1.6],
-      <title>Quote Machine of Sorts</title>
-  </div>
-]
+    [9, 'Canada 3430, 3710, 3917, 3905, 3906']
+];
+
