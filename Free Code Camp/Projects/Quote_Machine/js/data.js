f137ef00ad7dabaddabc9d1af95f287012d0ac35
James Walker <james.walker@contractor.net>
2017-12-04 23:32:19 -0600

    Editing array TariffText array info

--- Free Code Camp/Projects/Quote_Machine/js/data.js
+++ Free Code Camp/Projects/Quote_Machine/js/data.js
@@ -4,8 +4,7 @@ const tariffText = [
     [2, 'China', '1,083', '1,242',	'1,243', '1,350', '1,231', '-119', '-8.8'],
     [3, 'India', 141, 162, 167],
     [4, 'Canada', 3430, 3710, 3917, 3905, 3906],
- 
-    Total domestic exports	17,366	19,475	19,251	19,705	19,992	287	1.5
+    [5, 'Total domestic exports	17366	19475	19251	19705	19992	287	1.5'],
     Re-exports	2,350	2,897	3,221	3,619	3,924	305	8.4
     Total U.S. exports (domestic exports and re-exports)	19,716	22,372	22,472	23,324	23,916	592	2.5
     China	42,195	44,884	45,066	46,469	47,204	735	1.6]
@@ -13,11 +12,8 @@ const tariffText = [
   <link rel="stylesheet" type="text/css" href="css/style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <img src="img/cookie.jpg" alt="Fortune cookie fortune display" usemap="#fortune"/>
-  <map name="fortune">
     <area shape="rect" coords="0,0,1500,999" id="clicker">
   </map>
-  <div id="fortuneBox">
-    A Smile is Your Passport into the hearts of others
   </div>
  
 ]
