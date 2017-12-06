57e5d4bb8365253b4a11315270429a2c56759395
James Walker <james.walker@contractor.net>
2017-12-05 23:49:55 -0600

    Kata - Simplle String Indices

---
+++ CodeWars Katas/Simple_String_Indices.js
@@ -0,0 +1,1 @@
+In this Kata, you will be given a string with brackets and an index of an opening bracket and your task will be to return the index of the matching closing bracket. Return -1 if there is no answer. An opening brace will always have a closing brace.
