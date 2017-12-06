863d19da7996a247aeff452fcc55d14aa932a257
James Walker <james.walker@contractor.net>
2017-12-06 10:43:32 -0600

    fixed = in comparison

--- CodeWars Katas/Simple_String_Indices.js
+++ CodeWars Katas/Simple_String_Indices.js
@@ -2,9 +2,9 @@
 // and your task will be to return the index of the matching closing bracket. Return -1 if 
 // there is no answer. An opening brace will always have a closing brace. For example:
 //
-solve("((1)23(45))(aB)", 0) = 10 -- the opening brace at index 0 matches the closing brace at index 10
-solve("((1)23(45))(aB)", 1) = 3 
-solve("((1)23(45))(aB)", 2) = -1 -- there is no opening bracket at index 2, so return -1
-solve("((1)23(45))(aB)", 6) = 9
-solve("((1)23(45))(aB)", 11) = 14
-solve("((>)|?(*'))(yZ)", 11) = 14
+// solve("((1)23(45))(aB)", 0) = 10 -- the opening brace at index 0 matches the closing brace at index 10
+// solve("((1)23(45))(aB)", 1) = 3 
+// solve("((1)23(45))(aB)", 2) = -1 -- there is no opening bracket at index 2, so return -1
+// solve("((1)23(45))(aB)", 6) = 9
+// solve("((1)23(45))(aB)", 11) = 14
+// solve("((>)|?(*'))(yZ)", 11) = 14
