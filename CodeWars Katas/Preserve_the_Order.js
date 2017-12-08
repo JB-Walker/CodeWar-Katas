45fddb06ab5144798196d6639db33cf20b321948
James Walker <james.walker@contractor.net>
2017-12-07 23:11:39 -0600

    new kata - preserve the order comments entered

---
+++ CodeWars Katas/Preserve_the_Order.js
@@ -0,0 +1,9 @@
+// Your task is to design a function encode that takes a Number and 
+// return a string representation of this number. All numbers should have a representation 
+// such that the order is preserved when working with string representation
+// All Numbers should be encodable, except for NaN, since it is not "orderable" 
+// with other numbers. For numbers with different representations (eg 1e3 and 1000), 
+// each of them should encode to the same string representation.
+// Then, design a function decode that takes a valid string representation of a number 
+// and return the number.
+//
