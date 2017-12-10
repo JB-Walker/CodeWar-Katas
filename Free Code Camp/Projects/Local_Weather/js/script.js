08f0727cc42fc334ec5a653caa8442f954e8b229
James Walker <james.walker@contractor.net>
2017-12-09 22:59:03 -0600

    using geolocation

--- Free Code Camp/Projects/Local_Weather/js/script.js
+++ Free Code Camp/Projects/Local_Weather/js/script.js
@@ -1,5 +1,10 @@
-$(document).ready(function() {    
+$(document).ready(function() {
+    var api = "https://fcc-weather-api.glitch.me/api/current?";
+    var lat, lon;
+    var tempUnit = 'C';
+    var currentTempInCelsius;
+    
     if (navigator.geolocation) {
-        
+
     }
 }
