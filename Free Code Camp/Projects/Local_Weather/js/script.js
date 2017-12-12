3f788557c1fda43d2e03b6d70523d3f05eb9449c
James Walker <james.walker@contractor.net>
2017-12-11 20:42:34 -0600

    starting project over

--- Free Code Camp/Projects/Local_Weather/js/script.js
+++ Free Code Camp/Projects/Local_Weather/js/script.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
-    var api = "https://fcc-weather-api.glitch.me/api/current?";
-    var tempUnit = 'F';
-    var currentTempInfahrenheit;
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-        var lat = "lat=" + position.coords.latitude;
-        var lon = "lon=" + position.coords.longitude;
-        getWeather(lat, lon);
-        });
-    } 
 }
