7aeaf7048a3122d16469daecdd0b44b20465f955
James Walker <james.walker@contractor.net>
2017-12-13 22:15:23 -0600

    Lesson 308

--- Free Code Camp/Lessons/Lessons.js
+++ Free Code Camp/Lessons/Lessons.js
@@ -1769,3 +1769,7 @@ truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "ma
 // 307: JavaScript Lingo - Regular Expressions
 // Q1: RegExp can be a useful tool when working with strings. True.
 // Q2: RegExp can be intimidating, and if you don't like it, you can probably just avoid using it. False.
+
+// 308: Chrome Dev Tools - Elements
+// Q1: The elements tab is where you should go if you want to edit HTML and CSS of an existing web page. True.
+// Q2: Any changes you make are saved, so it's safe to use as an editing tool. False.
