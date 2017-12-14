16e289d4fae2d24255621c97c7c716164e974f79
James Walker <james.walker@contractor.net>
2017-12-13 23:33:49 -0600

    Beta Lesson 3 - wip

--- Free Code Camp/Lessons/beta.js
+++ Free Code Camp/Lessons/beta.js
@@ -12,3 +12,50 @@ function checkSign(num) {
     return num === 0 ? "zero" : (num > 0) ? "positive" : "negative";
 }
 checkSign(10);
+
+// 3. Basic JavaScript: Profile Lookup
+// We have an array of objects representing different people in our contacts lists.
+// A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
+// The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
+// If both are true, then return the "value" of that property.
+// If name does not correspond to any contacts then return "No such contact"
+// If prop does not correspond to any valid properties of a contact found to match name then return "No such property"
+//
+// wip - 1st attempt
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+function lookUpProfile(name, prop){
+    const data = contacts.find( function( ele ) { 
+      return ele.firstname === name;
+    });
+    if (data) {
+      return data.hasOwnProperty(prop) ? data.prop : "No such property";
+    } else {
+      return "No such contact";
+    }
+}
+lookUpProfile("Akira", "likes");
