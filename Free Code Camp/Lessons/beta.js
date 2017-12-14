e9a85379c3fc84bc22f87662b4b767340980da95
James Walker <james.walker@contractor.net>
2017-12-13 22:29:27 -0600

    Beta Lesson 1

---
+++ Free Code Camp/Lessons/beta.js
@@ -0,0 +1,7 @@
+// 1. Basic JavaScript: Use the Conditional (Ternary) Operator
+// Use the conditional operator in the checkEqual function to check if two numbers are equal or not. 
+// The function should return either true or false.
+function checkEqual(a, b) {
+    return a === b ? true : false;
+}
+checkEqual(1, 2);
