2de58868037a5845c2f515d1996dec9b77709937
James Walker <james.walker@contractor.net>
2017-12-15 23:49:17 -0600

    Beta Lesson 6 - completed

--- Free Code Camp/Lessons/beta.js
+++ Free Code Camp/Lessons/beta.js
@@ -113,4 +113,18 @@ console.log(printNumTwo());
 // returns 2
 console.log(i);
 // returns "i is not defined"
+//
+// Fix the code so that i declared in the if statement is a separate variable than i declared in the first line of 
+// the function. Be certain not to use the var keyword anywhere in your code.
+'use strict';
+function checkScope() {
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+checkScope();
 
