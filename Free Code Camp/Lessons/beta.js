e521025e357d1b907d87163a17809d81eddaf6d7
James Walker <james.walker@contractor.net>
2017-12-17 23:47:40 -0600

    Beta Lesson 11

--- Free Code Camp/Lessons/beta.js
+++ Free Code Camp/Lessons/beta.js
@@ -181,4 +181,8 @@ console.log(magic());
 
 // 11. ES6: Write Arrow Functions with Parameters
 // Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
-// 
+'use strict';
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+
