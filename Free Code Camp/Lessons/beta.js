c53021b287445c61e0d5b7fc1eb7f6851f3c1841
James Walker <james.walker@contractor.net>
2017-12-19 23:53:04 -0600

    Beta 20 - instructions & problem

--- Free Code Camp/Lessons/beta.js
+++ Free Code Camp/Lessons/beta.js
@@ -259,6 +259,19 @@ console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
 
 // 20. ES6: Use Destructuring Assignment to Pass an Object as a Function's Parameters
 // Use destructuring assignment within the argument to the function half to send only max and min inside the function.
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+// change code below this line
+const half = (stats) => ((stats.max + stats.min) / 2.0); // use function argument destructurung
+// change code above this line
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015
 
 
 
