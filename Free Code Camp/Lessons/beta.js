7777e4e017c65f3df09a3e31275007c0f45fd12b
James Walker <james.walker@contractor.net>
2017-12-19 23:49:42 -0600

    Beta 19 - instructions & problem

--- Free Code Camp/Lessons/beta.js
+++ Free Code Camp/Lessons/beta.js
@@ -250,6 +250,13 @@ console.log(b); // should be 8
 // 19. ES6: Use Destructuring Assignment with the Rest Operator to Reassign Array Elements
 // Use destructuring assignment with the rest operator to perform an effective Array.prototype.slice() 
 // so that arr is a sub-array of the original array source with the first two elements ommitted.
+const source = [1,2,3,4,5,6,7,8,9,10];
+// change code below this line
+const arr = source; // change this
+// change code below this line
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
+
 
 
 
