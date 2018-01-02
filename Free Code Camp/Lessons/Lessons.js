08d8213c01e4408d31d800714101dcfeca3f3792
James Walker <james.walker@contractor.net>
2018-01-01 22:53:36 -0600

    edited lessons

--- Free Code Camp/Lessons/Lessons.js
+++ Free Code Camp/Lessons/Lessons.js
@@ -1280,7 +1280,7 @@ function whatIsInAName(collection, source) {
 }
 whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
 
-// 257: Search and Replace
+// 257. Search and Replace
 // Perform a search and replace on the sentence using the arguments provided and return the new sentence.
 // First argument is the sentence to perform the search and replace on. Second argument is the word that
 // you will be replacing (before). Third argument is what you will be replacing the second argument with (after).
