37f1f122f508f555829fbd70056c1f9293ecca56
James Walker <james.walker@contractor.net>
2018-01-01 22:53:20 -0600

    edited lessons

--- Free Code Camp/Lessons/Lessons.js
+++ Free Code Camp/Lessons/Lessons.js
@@ -1294,7 +1294,7 @@ function myReplace(str, before, after) {
 }
 myReplace("He is Sleeping on the couch", "Sleeping", "sitting");
 
-// 258: Pig Latin
+// 258. Pig Latin
 // Translate the provided string to pig latin. Pig Latin takes the first consonant
 // (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
 // If a word begins with a vowel you just add "way" to the end. Input strings are guaranteed to be
