87c8d7537155664c8ae512e05201b1b6e3460d2d
James Walker <james.walker@contractor.net>
2018-01-01 22:52:09 -0600

    edited lessons

--- Free Code Camp/Lessons/Lessons.js
+++ Free Code Camp/Lessons/Lessons.js
@@ -1353,7 +1353,7 @@ function booWho(bool) {
 }
 booWho(null);
 
-// 262: Sorted Union
+// 262. Sorted Union
 // Write a function that takes two or more arrays and returns a new array of unique values 
 // in the order of the original provided arrays. In other words, all values present from all
 // arrays should be included in their original order, but with no duplicates in the final array.
