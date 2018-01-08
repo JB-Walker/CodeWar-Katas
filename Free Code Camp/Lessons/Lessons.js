8caeef975762ca6afb212cdb0a52f86cad1f0f5b
James Walker <james.walker@contractor.net>
2018-01-07 22:47:43 -0600

    added titles and instructions

--- Free Code Camp/Lessons/Lessons.js
+++ Free Code Camp/Lessons/Lessons.js
@@ -19,7 +19,11 @@ var lastLetterOfLastName = lastName[lastName.length - 1];
 var secondToLastLetterOfLastName = lastName[lastName.length - 2];
 
 // 149. Word Blanks
-// 
+// In this challenge, we provide you with a noun, a verb, an adjective and an adverb. You need to form a complete sentence 
+// using words of your choice, along with the words we provide. You will need to use the string concatenation operator + to 
+// build a new string, using the provided variables: myNoun, myAdjective, myVerb, and myAdverb. You will then assign the 
+// formed string to the result variable. You will also need to account for spaces in your string, so that the final sentence 
+// has spaces between all the words. The result should be a complete sentence.
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
     var result = "";
     result = "The huge, " + myAdjective + " " + myNoun + " " + myVerb + " around the corner " + myAdverb + "!!";
