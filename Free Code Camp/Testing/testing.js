89f592bc1d97a15b0ea759219ada05f6c12475f6
James Walker <james.walker@contractor.net>
2018-01-09 22:25:26 -0600

    testing

--- Free Code Camp/Testing/testing.js
+++ Free Code Camp/Testing/testing.js
@@ -1,14 +1,18 @@
 function whatIsInAName(collection, source) {
   function whatIsInAName(collection, source) {
     var matchedObjects = [];
-    collection.forEach(function(objectArgument) {
-      var objectProps = Object.keys(objectArgument);
+    function whatIsInAName(collection, source) {
+      function whatIsInAName(collection, source) {
+        var matchedObjects = [];
         collection.forEach(function(objectArgument) {
           var objectProps = Object.keys(objectArgument);
           for (var key in source) {
-            if (objectProps.includes(key) && objectArgument[key] === source[key]) {
-            }
-          }
+            if (objectProps.includes(key) && o
+        });
+        return matchedObjects;
+      }
+      whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+      
         });
         return matchedObjects;
       }
