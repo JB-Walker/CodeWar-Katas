a109be1dc873838e0e3755080d8cb627ec74177d
James Walker <james.walker@contractor.net>
2018-01-09 22:27:58 -0600

    testing

--- Free Code Camp/Testing/testing.js
+++ Free Code Camp/Testing/testing.js
@@ -34,18 +34,9 @@ function whatIsInAName(collection, source) {
       }
       whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
       
-        collection.forEach(function(objectArgument) {
-          var objectProps = Object.keys(objectArgument);
-          for (var key in source) {
-            if (objectProps.includes(key) && objectArgument[key] === source[key]) {
-            }
-          }
-        });
-        return matchedObjects;
-      }
-      whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
       function whatIsInAName(collection, source) {
         function whatIsInAName(collection, source) {
+          var matchedObjects = [];
           collection.forEach(function(objectArgument) {
             var objectProps = Object.keys(objectArgument);
             for (var key in source) {
@@ -55,6 +46,5 @@ function whatIsInAName(collection, source) {
           });
           return matchedObjects;
         }
-        ([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
-  
+        whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
         
