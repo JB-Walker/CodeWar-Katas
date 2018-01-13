09aaddb279f34f31079e0f8d23fd3e4d5aef8dbd
James Walker <james.walker@contractor.net>
2018-01-12 23:16:13 -0600

    testing

--- Free Code Camp/Testing/testing.js
+++ Free Code Camp/Testing/testing.js
@@ -9,9 +9,20 @@ sInAName([{
   whatIsInAName(
     collection,
      source) {
-  sInAName([{ 
-    first: 
-    "Romeo", 
-  last: 
-  "Montague" }, "Montague" }, 
+      function 
+      whatIsInAName(
+        collection,
+         source) {
+      sInAName([{ 
+        first: 
+        "Romeo", 
+        function 
+        whatIsInAName(
+          collection,
+           source) {
+        sInAName([{ 
+          first: 
+          "Romeo", 
+        last: 
+        "Montague" }, "Montague" },
                                                                                   
