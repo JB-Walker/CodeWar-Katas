457ac0b7f33451db9ec3e0ecbf3b2514e0f2eba0
James Walker <james.walker@contractor.net>
2018-01-13 21:17:07 -0600

    Lesson 316 title and instructions

--- Free Code Camp/Lessons/Lessons.js
+++ Free Code Camp/Lessons/Lessons.js
@@ -1817,4 +1817,11 @@ truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "ma
 // Q1. The console in Chrome Dev Tools has many similarities to the console of a computer. True.
 // Q2. This video is in-depth and covers everything you need to know. False.
 
+// 316. Arguments Optional
+// Create a function that sums two arguments together. If only one argument is provided, then return a function that 
+// expects one argument and returns the sum. For example, addTogether(2, 3) should return 5, and addTogether(2) should return a function.
+// Calling this returned function with a single argument will then return the sum:
+// var sumTwoAnd = addTogether(2);
+// sumTwoAnd(3) returns 5.
+// If either argument isn't a valid number, return undefined.
 
