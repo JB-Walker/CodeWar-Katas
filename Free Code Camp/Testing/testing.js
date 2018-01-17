90749b8296549a1895e47b17738efab74b8c35a3
James Walker <james.walker@contractor.net>
2018-01-16 22:55:06 -0600

    testing

--- Free Code Camp/Testing/testing.js
+++ Free Code Camp/Testing/testing.js
@@ -7,3 +7,68 @@ Front-End Development Certification<br>
   <li>31/31 Responsive Design with Bootstrap</li>
   <li>18/18 jQuery</li>
 
+  ```html
+  02/03 Basic Front-End Development Projects ###### Skipped 1 ######
+  ```
+
+  <li>104/104 Basic JavaScript</li>
+  <li>13/13 Object Oriented and Functional Programming</li>
+  <li>17/17 Basic Algorithm Scripting</li>
+  <li>07/07 JSON APIs and AJAX</li>
+
+  ```html
+  01/04 Intermediate Front End Development Projects ###### Skipped 3 ######
+  ```
+
+  <li>20/21 Intermediate Algorithm Scripting</li>
+  <li>00/09 Advanced Algorithm Scripting</li>
+
+  ```html
+  00/04 Advanced Front End Development Projects
+  ```
+
+</ul>
+<br>
+
+Data Visualization Certification<br>
+<ul>
+
+  ```html
+  00/05 React Projects
+  00/05 Data Visualization Projects
+  ```
+
+</ul>
+<br>
+
+Back-End Development Certification<br>
+<ul>
+  <li>00/02 Automated Testing and Debugging</li>
+  <li>00/01 Git</li>
+  <li>00/05 Node.js and Express.js</li>
+  <li>00/01 MongoDB</li>
+
+  ```html
+  00/06 API Projects
+  00/06 Dynamic Web Application Projects
+  ```
+
+</ul>
+<br>
+
+<strong>
+=============<br>
+02/32 Total Projects<br>
+=============<br>
+</strong>
+<br>
+
+Video Challenges<br>
+<ul>
+  <li>22/22 Computer Basics</li>
+  <li>02/02 The DOM</li>
+  <li>09/09 JavaScript Lingo</li>
+  <li>00/09 Chrome Developer Tools</li>
+  <li>00/02 Big O Notation</li>
+</ul>
+<br>
